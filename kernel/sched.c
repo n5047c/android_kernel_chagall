a59621b6a9560d993e075958de89cbec16160185
franciscofranco <franciscofranco.1990@gmail.com>
2013-02-10 15:16:39 -0800

    sched: add averaging routines from codeaurora's msm-3.4. They are used on the hotplug driver.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -1809,6 +1809,7 @@ static void inc_nr_running(struct rq *rq)
 	write_seqcount_begin(&rq->ave_seqcnt);
 	rq->ave_nr_running = do_avg_nr_running(rq);
 	rq->nr_last_stamp = rq->clock_task;
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	rq->nr_running++;
 	write_seqcount_end(&rq->ave_seqcnt);
 }
@@ -1818,6 +1819,7 @@ static void dec_nr_running(struct rq *rq)
 	write_seqcount_begin(&rq->ave_seqcnt);
 	rq->ave_nr_running = do_avg_nr_running(rq);
 	rq->nr_last_stamp = rq->clock_task;
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 	rq->nr_running--;
 	write_seqcount_end(&rq->ave_seqcnt);
 }
