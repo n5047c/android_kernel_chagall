f2f520f2b9fec83a34c7e73d2abac9939477f57f
Вячеслав Роенко <roenko_vv@transset.ru>
2013-10-14 15:53:23 +0400

    block: elevator: add elevator_queue_empty_fn

--- include/linux/elevator.h
+++ include/linux/elevator.h
@@ -20,6 +20,7 @@ typedef void (elevator_bio_merged_fn) (struct request_queue *,
 typedef int (elevator_dispatch_fn) (struct request_queue *, int);
 
 typedef void (elevator_add_req_fn) (struct request_queue *, struct request *);
+typedef int (elevator_queue_empty_fn) (struct request_queue *);
 typedef int (elevator_reinsert_req_fn) (struct request_queue *,
 					struct request *);
 typedef bool (elevator_is_urgent_fn) (struct request_queue *);
@@ -49,6 +50,7 @@ struct elevator_ops
 	elevator_is_urgent_fn *elevator_is_urgent_fn;
 
 	elevator_activate_req_fn *elevator_activate_req_fn;
+	elevator_queue_empty_fn *elevator_queue_empty_fn;
 	elevator_deactivate_req_fn *elevator_deactivate_req_fn;
 
 	elevator_completed_req_fn *elevator_completed_req_fn;
