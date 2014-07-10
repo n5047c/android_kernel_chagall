9d5de3ef9826928c4157b780da242e0e08c288a1
Вячеслав Роенко <roenko_vv@transset.ru>
2014-07-10 10:31:10 +0400

    Revert "wifi: no wakelock"
    This reverts commit cdc66f55fa9693a30b0d2efea1d34f4a3b347099.

--- drivers/net/wireless/bcmdhd/bcmsdh_sdmmc_linux.c
+++ drivers/net/wireless/bcmdhd/bcmsdh_sdmmc_linux.c
@@ -178,13 +178,8 @@ MODULE_DEVICE_TABLE(sdio, bcmsdh_sdmmc_ids);
 static int bcmsdh_sdmmc_suspend(struct device *pdev)
 {
 	struct sdio_func *func = dev_to_sdio_func(pdev);
-#if 0
-	/* Part of keep power while host suspended,
-	 * and make a wakelock and prevent deep sleep! so it's disabled!
-	 */
 	mmc_pm_flag_t sdio_flags;
 	int ret;
-#endif
 
 	if (func->num != 2)
 		return 0;
@@ -193,10 +188,6 @@ static int bcmsdh_sdmmc_suspend(struct device *pdev)
 
 	if (dhd_os_check_wakelock(bcmsdh_get_drvdata()))
 		return -EBUSY;
-#if 0
-	/* why keep power while host suspended?
-	 * and make a wakelock and prevent deep sleep! so it's disabled!
-	 */
 	sdio_flags = sdio_get_host_pm_caps(func);
 
 	if (!(sdio_flags & MMC_PM_KEEP_POWER)) {
@@ -210,8 +201,6 @@ static int bcmsdh_sdmmc_suspend(struct device *pdev)
 		sd_err(("%s: error while trying to keep power\n", __FUNCTION__));
 		return ret;
 	}
-#endif
-
 #if defined(OOB_INTR_ONLY)
 	bcmsdh_oob_intr_set(0);
 #endif	/* defined(OOB_INTR_ONLY) */
