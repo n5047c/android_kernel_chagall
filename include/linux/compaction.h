d721644e5b0eefaf14ef8a021194000676ce8edb
Вячеслав Роенко <roenko_vv@transset.ru>
2014-10-03 17:14:08 +0400

    Revert "lowmemorykiller: Use asynchronous compaction"
    This reverts commit e88f97a32e6ba5d076178142493316deac96108b.

--- include/linux/compaction.h
+++ include/linux/compaction.h
@@ -76,7 +76,7 @@ static inline bool compaction_deferred(struct zone *zone)
 	return 1;
 }
 
-static inline int compact_nodes(bool sync)
+static inline int compact_nodes()
 {
 	return COMPACT_CONTINUE;
 }
--- mm/compaction.c
+++ mm/compaction.c
@@ -655,7 +655,7 @@ unsigned long try_to_compact_pages(struct zonelist *zonelist,
 
 
 /* Compact all zones within a node */
-static int compact_node(int nid, bool sync)
+static int compact_node(int nid)
 {
 	int zoneid;
 	pg_data_t *pgdat;
@@ -673,7 +673,6 @@ static int compact_node(int nid, bool sync)
 			.nr_freepages = 0,
 			.nr_migratepages = 0,
 			.order = -1,
-            .sync = sync,
 		};
 
 		zone = &pgdat->node_zones[zoneid];
@@ -694,16 +693,15 @@ static int compact_node(int nid, bool sync)
 }
 
 /* Compact all nodes in the system */
-int compact_nodes(bool sync)
+static int compact_nodes(void)
 {
 	int nid;
 
 	for_each_online_node(nid)
-		compact_node(nid, sync);
+		compact_node(nid);
 
 	return COMPACT_COMPLETE;
 }
-EXPORT_SYMBOL(compact_nodes);
 
 /* The written value is actually unused, all memory is compacted */
 int sysctl_compact_memory;
@@ -713,7 +711,7 @@ int sysctl_compaction_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *length, loff_t *ppos)
 {
 	if (write)
-		return compact_nodes(true);
+		return compact_nodes();
 
 	return 0;
 }
